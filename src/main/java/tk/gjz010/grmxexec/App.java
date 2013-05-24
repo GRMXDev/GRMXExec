537b3237dd4b172fae6987fcca0bb3cbc348ae51
gjz010 <gjz010944@gmail.com>
2013-05-24 20:31:34 +0800

    Init Commit

---
+++ src/main/java/tk/gjz010/grmxexec/App.java
@@ -0,0 +1,13 @@
+package tk.gjz010.grmxexec;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
